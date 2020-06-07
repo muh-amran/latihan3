7f0e84adfcc7a4a724e71bc33832d8a4ea54c9c8
Amran <46549975+allbugisi@users.noreply.github.com>
2020-06-08 05:22:12 +0800

    perbaikilogin coy

---
+++ auth/login.php
@@ -0,0 +1,39 @@
+<?php
+require './../layout/auth_header.php';
+
+?>
+<div id="content-wrapper" class="d-flex flex-column">
+  <div id="layoutAuthentication">
+    <div id="layoutAuthentication_content">
+      <main>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-5">
+              <div class="card shadow-lg border-0 rounded-lg mt-5">
+                <div class="card-header">
+                  <h3 class="text-center font-weight-light my-4">Login Page</h3>
+                </div>
+                <div class="card-body">
+
+                  <form method="POST" action="">
+
+                    <div class="form-group"><label class="small mb-1" for="inputUsername">Username</label><input class="form-control py-4" id="username" type="text" placeholder="Enter Username" name="username" value="" autofocus autocomplete="off" /></div>
+
+                    <div class="form-group"><label class="small mb-1" for="inputPassword">Password</label><input class="form-control py-4" id="password" type="password" placeholder="Enter password" name="password" autofocus autocomplete="off" /></div>
+
+                    <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0"><a class="small" href="password.html">Forgot Password?</a><button class="btn btn-primary" type="submit">Login</button></div>
+                  </form>
+                </div>
+                <div class="card-footer text-center">
+                  <div class="small"><a href="<?= $base_url; ?>auth/registeration">Need an account? Sign up!</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+    <?php
+    require './../layout/auth_footer.php';
+
+    ?>
